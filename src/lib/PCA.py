cd609bd6e6ea7a7b96f5a894f2a66f41042e0a8e
Saumya Singh <saumyakeyid@gmail.com>
2026-04-28 18:53:31 +0200

    PCA with explained variance ratio

--- src/lib/PCA.py
+++ src/lib/PCA.py
@@ -4,13 +4,17 @@ import numpy as np
 class PCA:
     def __init__(self, n_components):
         self.n_components = n_components
+        self.explained_variance_ratio_ = None
 
     def fit_transform(self, X):
         X = np.asarray(X)
         X_centered = X - X.mean(axis=0)
         U, s, Vt = np.linalg.svd(X_centered)
 
+        variance_ratio_ = s ** 2 / np.sum(s ** 2)
+
         W_d = Vt[:self.n_components].T
         X_d = X_centered @ W_d
+        self.explained_variance_ratio_ = variance_ratio_[:self.n_components]
 
         return X_d
